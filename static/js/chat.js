3fec782f77d1cdabcccfd8cccdaceb007fefce6a
David Esperanza <david.esperanza@ckluster.com>
2013-10-22 14:40:51 -0700

    css and socketio listener
    also added some eyecandy.

--- static/js/chat.js
+++ static/js/chat.js
@@ -2,12 +2,8 @@ $(function() {
     // expected url: http://location:5000/chat/{chatroom}
     var url = document.URL,
         chatroom = "",
-        // expected result: chat/{chatroom}
-        url = url.substr(url.indexOf('chat'));
-        if(url.indexOf('/')) {
-            chatroom = url.split('/')[1];
-        }
-
+        textarea = $('textarea'),
+        messages = $('.messages'),
         sendMessage = function() {
             var message = textarea.val();
             $.ajax({
@@ -18,12 +14,37 @@ $(function() {
                 }),
                 processData: false,
                 success: function(posted) {
-                    alert(posted);
+                    scrollToBottom();
                 },
                 contentType: "application/json",
                 dataType: "application/json"
             });
-        };
-    var textarea = $('textarea');
+        },
+        renderMessage = function(message) {
+            console.log('rendering message');
+            console.log(message);
+            scrollToBottom();
+        },
+        scrollToBottom  = function() {
+            messages[0].scrollTop = messages[0].scrollHeight;
+        }
+
+    // expected result: chat/{chatroom}
+    url = url.substr(url.indexOf('chat'));
+
+    if(url.indexOf('/')) {
+        chatroom = url.split('/')[1];
+    }
+
     $('input[type="button"]').click(sendMessage);
+    
+    // setup socketIO listener
+    var messagesSocket =  io.connect('http://localhost:5886');
+    messagesSocket.on('notifexample', function(data) {
+        console.log('notification received');
+        console.log('data');
+        renderMessage(data);
+    });
+    
+    scrollToBottom();
 });
